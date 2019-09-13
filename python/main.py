7b2515eaa090bcfe05a6343325826b5b06492388
Cristhian Bicca <cristhian.bicca@gmail.com>
2019-09-12 22:20:13 -0400

    Entende string

--- python/main.py
+++ python/main.py
@@ -3,16 +3,13 @@ def verificaVariavel(variavel):
     if re.match("\d+\.\d+", variavel):
         print("ACHEI FLOAT {}".format(variavel.strip()))
     else:
-        #if re.findall("\w+", variavel)
-        if re.findall("([A-Z]+[a-z]+)", variavel):
-            a = re.findall("([A-Z][a-z]+)", variavel)
-            print("ACHEI STRING {}".format(a))
+        if re.fullmatch("(^[a-zA-Z])+[0-9]*?[a-zA-Z0-9]*\n", variavel):
+            print("ACHEI STRING {}".format(variavel.strip()))
 filepath = 'file.txt'
 with open(filepath) as fp:
    line = fp.readline()
    cnt = 1
    while line:
-       #print("Line {}: {}".format(cnt, line.strip()))
        verificaVariavel(line)
        line = fp.readline()
        cnt += 1
