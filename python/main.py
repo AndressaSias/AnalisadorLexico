a03b76686f61ffdc36799b109b6ace01773d48af
Cristhian Bicca <cristhian.bicca@gmail.com>
2019-09-13 05:50:09 -0400

    Add id variable

--- python/main.py
+++ python/main.py
@@ -2,40 +2,42 @@ import re
 def verificaVariavel(variavel,cnt):
     if re.fullmatch("\d+\n", variavel):
         print("[{}] INT".format(cnt, variavel.strip()))
-    else:
-        if re.fullmatch("\d+\.\d+\n", variavel):
+        return "inteiro"
+    elif re.fullmatch("\d+\.\d+\n", variavel):
             print("[{}] FLOAT {}".format(cnt, variavel.strip()))
-        else:
-            if re.fullmatch("(^[a-zA-Z])+[0-9]*?[a-zA-Z0-9]*\n", variavel):
-                if variavel == "int\n":
-                    print("[{}] INT {}".format(cnt, variavel.strip()))
-                elif variavel == "double\n":
+            return "real"
+    elif re.fullmatch("(^[a-zA-Z])+[0-9]*?[a-zA-Z0-9]*\n", variavel):
+            if variavel == "int\n":
+                print("[{}] INT {}".format(cnt, variavel.strip()))
+            elif variavel == "double\n":
                     print("[{}] DOUBLE {}".format(cnt, variavel.strip()))
-                elif variavel == "float\n":
+            elif variavel == "float\n":
                     print("[{}] FLOAT {}".format(cnt, variavel.strip()))
-                elif variavel == "real\n":
+            elif variavel == "real\n":
                     print("[{}] REAL {}".format(cnt, variavel.strip()))
-                elif variavel == "break\n":
+            elif variavel == "break\n":
                     print("[{}] BREAK {}".format(cnt, variavel.strip()))
-                elif variavel == "case\n":
+            elif variavel == "case\n":
                     print("[{}] CASE {}".format(cnt, variavel.strip()))
-                elif variavel == "char\n":
+            elif variavel == "char\n":
                     print("[{}] CHAR {}".format(cnt, variavel.strip()))
-                elif variavel == "const\n":
+            elif variavel == "const\n":
                     print("[{}] CONST {}".format(cnt, variavel.strip()))
-                elif variavel == "continue\n":
+            elif variavel == "continue\n":
                     print("[{}] CONTINUE {}".format(cnt, variavel.strip()))
-                else:
-                    return("identificador")
             else:
-                if re.fullmatch("[/]{2}[ ]*[a-zA-Z0-9\s]*\n", variavel):
-                     print("[{}] COMENTARIO {}".format(cnt, variavel.strip()))
+                return("identificador")
+    elif re.fullmatch("[/]{2}[ ]*[a-zA-Z0-9\s]*\n", variavel):
+             print("[{}] COMENTARIO {}".format(cnt, variavel.strip()))
+    else:
+         return("erro")
 filepath = 'file.txt'
 with open(filepath) as fp:
    line = fp.readline()
    cnt = 1
    ident = 1
    d = {}
+   erros = []
    while line:
        retorno = verificaVariavel(line, cnt)
        if retorno == "identificador":
@@ -43,6 +45,22 @@ with open(filepath) as fp:
                d[line.strip()] = ident
                ident = ident + 1
            print("[{}] IDENTIFICADOR {} {}".format(cnt, line.strip(), d[line.strip()]))
+       elif retorno == "inteiro":
+           if line.strip() not in d.keys(): 
+               d[line.strip()] = ident
+               ident = ident + 1
+           print("[{}] NUMERO INTEIRO {} {}".format(cnt, line.strip(), d[line.strip()]))
+       elif retorno == "real":
+           if line.strip() not in d.keys():
+               d[line.strip()] = ident
+               ident = ident + 1
+           print("[{}] NUMERO REAL {} {}".format(cnt, line.strip(), d[line.strip()]))
+       elif retorno == "erro":
+            erros.append(cnt)
        line = fp.readline()
        cnt += 1
+
+print("O programa possui erros nas linhas {}".format(erros))
+print("O programa possui erros nas linhas {}".format(d.keys()))
+
 
