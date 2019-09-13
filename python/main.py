0ffadbafaa3dd645cc472e99257407368619a9f1
Cristhian Bicca <cristhian.bicca@gmail.com>
2019-09-13 04:42:30 -0400

    Add print line

--- python/main.py
+++ python/main.py
@@ -1,20 +1,22 @@
 import re
-def verificaVariavel(variavel):
-    if re.match("\d+\.\d+", variavel):
-        print("ACHEI FLOAT {}".format(variavel.strip()))
+def verificaVariavel(variavel,cnt):
+    if re.fullmatch("\d+\n", variavel):
+        print("[{}] INT".format(cnt, variavel.strip()))
     else:
-        if re.fullmatch("(^[a-zA-Z])+[0-9]*?[a-zA-Z0-9]*\n", variavel):
-            print("ACHEI STRING {}".format(variavel.strip()))
+        if re.fullmatch("\d+\.\d+\n", variavel):
+            print("[{}] FLOAT {}".format(cnt, variavel.strip()))
         else:
-            if re.fullmatch("[/]{2}[ ]*[a-zA-Z0-9\s]*\n", variavel):
-                 print("ACHEI STRING {}".format(variavel.strip()))
-                 return variavel
+            if re.fullmatch("(^[a-zA-Z])+[0-9]*?[a-zA-Z0-9]*\n", variavel):
+                print("[{}] IDENTIFICADOR {}".format(cnt, variavel.strip()))
+            else:
+                if re.fullmatch("[/]{2}[ ]*[a-zA-Z0-9\s]*\n", variavel):
+                     print("[{}] COMENTARIO {}".format(cnt, variavel.strip()))
 filepath = 'file.txt'
 with open(filepath) as fp:
    line = fp.readline()
    cnt = 1
    while line:
-       a = verificaVariavel(line)
+       a = verificaVariavel(line, cnt)
        
        line = fp.readline()
        cnt += 1
